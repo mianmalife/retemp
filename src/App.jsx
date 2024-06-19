f6d993bc0f31e90890369cdd498d2bbfff8e1252
zhangkx <zhangkx@winnerinf.com>
2024-06-19 11:11:19 +0800

    feat: matchUtilities 自定义字体大小

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import Button from './component/Button'
-import './App.css'
 
 const clx = 'flex justify-center text-3xl font-bold text-[#333] hover:text-pink-500'
 function App() {
@@ -8,6 +7,7 @@ function App() {
       <a href='https://tailwindcss.com/docs' target='_blank' className={clx}>
         Hello tailwindcss!
       </a>
+      <h1 className='fsizes-24'>hello world!</h1>
       <Button>Normal</Button>
       <Button type='danger' size='lg'>Danger</Button>
       <Button type='primary'>Primary</Button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,33 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin')
+
+const createFontSize = (start, end) => {
+  let obj = {}
+  for (let i = start; i <= end; i += 2) {
+    obj[i] = `${i}px`
+  }
+  return obj
+}
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fsizes: createFontSize(12, 50),
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(({ matchUtilities, theme }) => {
+      matchUtilities({
+        fsizes: value => ({
+          fontSize: value
+        })
+      }, {
+        values: theme('fsizes')
+      })
+    })
+  ],
 }
 
