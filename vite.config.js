b219955170c28bb3c0c46cd7336acae3a3a37f97
mianmalife <253590433@qq.com>
2024-06-26 23:10:23 +0800

    ci: 修改base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/retemp/',
+  base: '/react-template/',
   server: {
     host: '0.0.0.0'
   },
